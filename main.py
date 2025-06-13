5c1a540e6780499e065a32ee4960faf2daaaef4d
Juan1401123 <juansamadors@gmail.com>
2025-06-13 00:46:07 -0600

    update

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import ast
 import os
 
 
-pytesseract.pytesseract.tesseract_cmd =r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd =r'C:\Program Files\Tesseract-OCR\tesseract.exe' 
 
 
 inputimg='C:\\Users\\juana\\Documents\\FlyCowOCR\\img-input2'
@@ -89,7 +89,7 @@ def cortar_por_primera_linea_negra(img, umbral_negro=75, tolerancia_blancos_rati
         blancos = np.sum(img_np[y] > umbral_negro)
         if blancos <= tolerancia_blancos:
             if debug:
-                print(f"Línea negra encontrada en y = {y} con {blancos} píxeles blancos.")
+                print(f"black line search in y = {y} with {blancos} white pixels.")
             cortada = img.crop((0, y, width, height))
             return cortada
 
@@ -103,18 +103,30 @@ for filename in os.listdir(inputimg):
         continue
 
     print(f"Procesando: {filename}")
+    # openimg = Image.open(filepath).convert('RGB')
+    # openimg = scale_width_and_pad_down(openimg)
+    # openimg = openimg.convert('L')
+    # openimg = openimg.crop((0, 59, openimg.width, openimg.height))
+
+    # enhancer = ImageEnhance.Contrast(openimg)
+    # openimg = openimg.point(lambda p: 0 if p < 100 else 255)
+    # openimg = enhancer.enhance(2)
+    # openimg=cortar_por_primera_linea_negra(openimg)
+    # openimg = openimg.filter(ImageFilter.GaussianBlur(radius=.01))
+    # openimg=openimg.filter(ImageFilter.MedianFilter(size=1))
     openimg = Image.open(filepath).convert('RGB')
     openimg = scale_width_and_pad_down(openimg)
     openimg = openimg.convert('L')
     openimg = openimg.crop((0, 59, openimg.width, openimg.height))
 
     enhancer = ImageEnhance.Contrast(openimg)
-    openimg = openimg.point(lambda p: 0 if p < 100 else 255)
+    openimg = openimg.filter(ImageFilter.GaussianBlur(radius=.40))
+# openimg = openimg.point(lambda p: 0 if p < 100 else 255)
+    # openimg=openimg.filter(ImageFilter.MedianFilter(size=3))
     openimg = enhancer.enhance(2)
     openimg=cortar_por_primera_linea_negra(openimg)
-    # openimg = openimg.filter(ImageFilter.GaussianBlur(radius=.01))
-    openimg=openimg.filter(ImageFilter.MedianFilter(size=1))
     imgtpocr=openimg
+
 
 
 
@@ -122,15 +134,16 @@ for filename in os.listdir(inputimg):
     row_size=38
     row_less=-3
     pivotcolum=imgtpocr.crop((column_boundaries[5][0]-5, 0, column_boundaries[5][1]+15, openimg.height))
-    for x in range(7):
+    for x in range(16):
          width, height = pivotcolum.size
-         n_round=math.floor(height/row_size-2)  
+         n_round=math.floor(height/(row_size-2))  
          for e in range(n_round):
              top = e * row_size
              bottom = top + row_size
              row_pivotcolum = pivotcolum.crop((0, top+row_less, width, bottom+row_less))
              keyword=pytesseract.image_to_string(row_pivotcolum.resize((row_pivotcolum.width * 8, row_pivotcolum.height * 8), Image.LANCZOS), config = r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=No1/2"eIc,').strip().replace('\n',',') 
              if keyword in listpivot:
+                  print('keyword search in row less '+ str(row_less))
                   for i, (x0, x1) in enumerate(column_boundaries):
                     if x0+10 < x1 and i==1:
                      col_crop = imgtpocr.crop((x0-38, 0, x1+15, openimg.height))
@@ -139,37 +152,39 @@ for filename in os.listdir(inputimg):
                     elif x0+10 < x1:
                      col_crop = imgtpocr.crop((x0-5, 0, x1+15, openimg.height))
                     width, height = col_crop.size
-                    for e in range(n_round):
-                       row_crop = col_crop.crop((0, top+row_less, width, bottom+row_less)) 
-                       if i==0:
-                          element1 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 5, row_crop.height * 5), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng').strip().replace('\n',',')
-                          e1.append(element1)
-                       elif i==1:
-                           element2 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 5, row_crop.height * 5), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng').strip()
-                           e2.append(element2)   
-                       elif i==2:
-                           element3 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 5, row_crop.height * 5), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',')  
-                           e3.append(element3)   
-                       elif i==3:
-                           element4 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 5, row_crop.height * 5), Image.LANCZOS), config=r'--psm 7 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',') 
-                           e4.append(element4)      
-                       elif i==4:
-                           element5 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 5, row_crop.height * 5), Image.LANCZOS), config=r'--psm 7 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',') 
-                           e5.append(element5)      
-                       elif i==5:
-                           element6 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 8, row_crop.height * 8), Image.LANCZOS), config = r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=No1/2"eIc,').strip().replace('\n',',')    
-                           e6.append(element6)   
-                       elif i==6:
-                           element7 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 5, row_crop.height * 5), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',') 
-                           e7.append(element7)      
-                       elif i==7: 
-                           element8 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 8, row_crop.height * 8), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',') 
-                           e8.append(element8)   
-                       elif i==8:           
-                           element9 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 8, row_crop.height * 8), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',') 
-                           e9.append(element9) 
-                       print(x,i)
-         row_less+=3  
+                    
+                    row_crop = col_crop.crop((0, top+row_less, width, bottom+row_less)) 
+                    if i==0:
+                       element1 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 10, row_crop.height * 10), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng').strip().replace('\n',',')
+                       e1.append(element1)
+                    elif i==1:
+                        element2 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 5, row_crop.height * 5), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=ABCDFromLegNn ').strip()
+                        e2.append(element2)   
+                    elif i==2:
+                        element3 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 5, row_crop.height * 5), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',')  
+                        e3.append(element3)   
+                    elif i==3:
+                        element4 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 5, row_crop.height * 5), Image.LANCZOS), config=r'--psm 7 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',') 
+                        e4.append(element4)      
+                    elif i==4:
+                        element5 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 5, row_crop.height * 5), Image.LANCZOS), config=r'--psm 7 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',') 
+                        e5.append(element5)      
+                    elif i==5:
+                        element6 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 8, row_crop.height * 8), Image.LANCZOS), config = r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=No1/2"eIc,').strip().replace('\n',',')    
+                        e6.append(element6)   
+                    elif i==6:
+                        element7 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 5, row_crop.height * 5), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',') 
+                        e7.append(element7)      
+                    elif i==7: 
+                        element8 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 8, row_crop.height * 8), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',') 
+                        e8.append(element8)   
+                    elif i==8:           
+                        element9 = pytesseract.image_to_string(row_crop.resize((row_crop.width * 8, row_crop.height * 8), Image.LANCZOS), config=r'--psm 6 --oem 3 -l eng -c tessedit_char_whitelist=0123456789.').strip().replace('\n',',') 
+                        e9.append(element9) 
+                    print('Spin '+ str(x), 'column '+ str(i), 'page '+ str(filename), 'row less: '+ str(row_less))
+
+         row_less+=1  
+    
     
 
 
